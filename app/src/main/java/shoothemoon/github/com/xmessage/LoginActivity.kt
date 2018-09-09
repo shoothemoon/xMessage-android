9aa8a91b24610fa432d6b8bd8959af41d85a2760
shoothemoon <ethandotadams@gmail.com>
2018-09-08 23:52:51 -0400

    updating register and login UI

---
+++ app/src/main/java/shoothemoon/github/com/xmessage/LoginActivity.kt
@@ -0,0 +1,16 @@
+package shoothemoon.github.com.xmessage
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+
+
+}
--- app/src/main/java/shoothemoon/github/com/xmessage/MainActivity.kt
+++ app/src/main/java/shoothemoon/github/com/xmessage/MainActivity.kt
@@ -1,12 +1,30 @@
 package shoothemoon.github.com.xmessage
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = register_Email.text.toString()
+        val password = register_Password.text.toString()
+
+        button_Register.setOnClickListener {
+            println("Email: $email, Password: $password")
+        }
+
+        alrdy_hve_acct.setOnClickListener {
+            Log.d("MainActivity", "Clicked to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
